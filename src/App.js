b9a8f423917665ff04d3f84fde09381070b760f0
Carrington Jones <carrington.jones47@gmail.com>
2021-10-08 22:54:13 -0500

    Cont. working on react movie application. Movie list now rendered on application.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
+import React, {Component} from "react";
+import Movies from "./components/movies"
 import './App.css';
 
-function App() {
-  return (
-      <main className="container">
-        <h1>Hello World</h1>
-      </main>
-  );
+class App extends Component {
+    render() {
+        return (
+            <main className="container">
+              <Movies />
+            </main>
+        );
+    }
 }
 
 export default App;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -5,12 +5,31 @@ class Movies extends Component {
     state = {
         movies: getMovies()
     };
-    handleDelete = movie => {
+    handleDelete = (movie) => {
 
     }
 
     render() {
-
+        return <table className="table">
+            <thead>
+            <tr>
+                <th>Title</th>
+                <th>Genre</th>
+                <th>Stock</th>
+                <th>Rate</th>
+            </tr>
+            </thead>
+            <tbody>
+            { this.state.movies.map(movie =>(
+            <tr>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+            </tr>
+                ))}
+            </tbody>
+        </table>
     }
 }
 
