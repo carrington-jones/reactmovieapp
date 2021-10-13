0669c43629ffe70ba46dfe00e4f3f2b355bcc122
Carrington Jones <carrington.jones47@gmail.com>
2021-10-12 22:25:53 -0500

    Added basic pagination to application

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,13 @@
 import React, {Component} from "react";
 import {getMovies} from "../services/fakeMovieService";
+import pagination from "./common/pagination";
 import Like from "./common/Like";
+import Pagination from "./common/pagination";
 
 class Movies extends Component {
     state = {
-        movies: getMovies()
+        movies: getMovies(),
+        pageSize: 4
     };
     handleDelete = (movie) => {
        const movies = this.state.movies.filter(m => m._id !== movie._id) ;
@@ -17,6 +20,10 @@ class Movies extends Component {
         movies[index] = {...movies[index]};
         movies[index].liked = !movies[index].liked;
         this.setState({movies});
+    }
+
+    handlePageChange = page => {
+        console.log(page);
     }
 
     render() {
@@ -54,6 +61,7 @@ class Movies extends Component {
                     ))}
                     </tbody>
                 </table>
+                <Pagination itemsCount={count} pageSize={this.state.pageSize} onPageChange={this.handlePageChange}/>
             </React.Fragment>
         )}
 }
