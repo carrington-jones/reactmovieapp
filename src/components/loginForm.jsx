1ac16e43838462633ab13fd0433b808578e1b2b2
Carrington Jones <carrington.jones47@gmail.com>
2021-10-20 22:06:29 -0500

    Application can now handle multiple inputs (username and password)

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -2,30 +2,49 @@ import React, {Component} from "react";
 
 class LoginForm extends Component {
 
-    username = React.createRef();
+    state = {
+        account: {username: '', password: ''}
+    }
 
-    // componentDidMount() {
-    //     this.username.current.focus();
-    // }
-
-    handleSubmit = e =>{
+    handleSubmit = e => {
         e.preventDefault()
     }
 
-
+    handleChange = ({ currentTarget: input}) => {
+        const account = {...this.state.account};
+        account[input.name] = input.value;
+        this.setState({account})
+    }
 
     render() {
+        const {account} = this.state;
+
         return (
             <div>
                 <h1>Login</h1>
                 <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
                         <label htmlFor="username">Username</label>
-                        <input autoFocus id="username" ref={this.username} type="text" className="form-control"/>
+                        <input
+                            autoFocus
+                            value={account.username}
+                            onChange={this.handleChange}
+                            id="username"
+                            name="username"
+                            type="text"
+                            className="form-control"
+                        />
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Password</label>
-                        <input id="password" type="text" className="form-control"/>
+                        <input
+                            value={account.password}
+                            onChange={this.handleChange}
+                            id="password"
+                            name="password"
+                            type="text"
+                            className="form-control"
+                        />
                     </div>
                     <button className="btn btn-primary">Login</button>
                 </form>
